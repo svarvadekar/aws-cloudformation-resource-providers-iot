e9b4f7aa6ae3de135235fe667b8b23165c730491
Smruti Varvadekar <svarvade@amazon.com>
2021-03-18 10:29:22 -0700

    Handle the AccessDenied exception when it gets thrown as IotException

--- aws-iot-dimension/src/test/java/com/amazonaws/iot/dimension/TranslatorTest.java
+++ aws-iot-dimension/src/test/java/com/amazonaws/iot/dimension/TranslatorTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import software.amazon.awssdk.services.iot.model.IndexNotReadyException;
+import software.amazon.awssdk.services.iot.model.IotException;
 import software.amazon.awssdk.services.iot.model.LimitExceededException;
 import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
@@ -23,6 +25,17 @@ public class TranslatorTest {
         HandlerErrorCode result = Translator.translateExceptionToErrorCode(
                 IndexNotReadyException.builder().build(), mock(Logger.class));
         assertThat(result).isEqualTo(HandlerErrorCode.InternalFailure);
+    }
+
+    @Test
+    public void translateIotExceptionToCfn_AccessDeniedErrorCode() {
+        HandlerErrorCode result =
+                Translator.translateExceptionToErrorCode(IotException.builder().statusCode(403)
+                        .message("User not authorised to perform on resource with an explicit deny " +
+                                "(Service: Iot, Status Code: 403, Request ID: dummy, " +
+                                "Extended Request ID: null), stack trace")
+                        .build(), mock(Logger.class));
+        assertThat(result).isEqualByComparingTo(HandlerErrorCode.AccessDenied);
     }
 
     @Test
